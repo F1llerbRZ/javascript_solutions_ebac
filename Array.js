b7061e570f3ee3f9f6cc54220e030c4fe3fb30fa
Thiago "F1ller" Oliveira <130257368+F1llerbRZ@users.noreply.github.com>
2024-02-22 01:18:30 -0300

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,10 @@
+var array = [10, 20, 30, 40, 50] 
+
+
+  var max = Math.max(...array);
+  var min = Math.min(...array);
+  
+console.log(max);
+console.log(min);
+
+module.exports = { array, max, min }
