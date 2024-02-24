da1969aecec23d9a86bda928c4d635e214fb3c3b
Thiago "F1ller" Oliveira <130257368+F1llerbRZ@users.noreply.github.com>
2024-02-24 02:08:02 -0300

    Delete MDC.test.js

--- MDC.test.js
+++
@@ -1,9 +0,0 @@
-const { mdc } = require ('../EX_20/MDC')
-
-describe('Validar resultado do MDC', () => {
-
-    it('Calculo com valores corretos', () => {
-        expect(mdc(6, 8)).toBe(2)
-    });
-    
-});   
